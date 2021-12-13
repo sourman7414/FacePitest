c2dee5c1c6ae3dc58601bd215a3b86ce4ba2dae6
Sourman7414 <unknowbird87@gmail.com>
2021-12-13 10:38:10 +0800

    ver 0.0.0

---
+++ face.py
@@ -0,0 +1,32 @@
+import fire , json
+
+class Facepi:
+    def readConfig(self):
+        with open('Config.json', 'r', encoding='utf-8') as f:
+            config = json.load(f)
+            return config
+    
+    def self(self):
+        print(self.readConfig())
+        config = self.readConfig()
+        print(config['api_key'])
+    
+    def writeconfig(self,config):
+        with open('Config.json','w', encoding='utf-8') as f:
+            json.dump(config,f)
+
+    def setconfig(self):
+        config = self.readConfig()
+        print("不更改config請按enter鍵")
+        api_key = input(f"輸入更改的api_key[{config['api_key']}]: ")
+        if api_key:  config['api_key'] = api_key
+        title = input(f"輸入更改的title[{config['title']}]: ")
+        if title: config['title'] = title 
+        self.writeconfig(config)
+
+
+if __name__=="__main__":
+    fire.Fire(Facepi)
+
+
+
